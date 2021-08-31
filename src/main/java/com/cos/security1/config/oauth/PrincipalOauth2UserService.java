14a442f3d9e39a538ea1e5de845a1851af493e8b
fj2008 <fj2008@naver.com>
2021-08-31 10:01:08 +0900

    페이스북 로그인 구현완료

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -10,6 +10,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.cos.security1.config.auth.PrincipalDetails;
+import com.cos.security1.config.oauth.provider.FacebookUserInfo;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2Userinfo;
 import com.cos.security1.model.User;
 import com.cos.security1.model.UserRepository;
 @Service
@@ -34,20 +37,30 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 		//구글로그인버튼 클릭->구글로그인창->로그인을 완료->code를 리턴(OAuth-Client라이브러리)->AccessToken요청
 		//userRequest정보->loadUser함수 호출->구글으로 부터 회원프로필 받아준다.
 		System.out.println("getAttributes"+oAuth2User.getAttributes());
+		OAuth2Userinfo oAuth2Userinfo=null;
+		if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("구글 로그인 요청");
+			oAuth2Userinfo = new GoogleUserInfo(oAuth2User.getAttributes());
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+			System.out.println("페이스북 로그인 요청");
+			oAuth2Userinfo = new FacebookUserInfo(oAuth2User.getAttributes());
+		}else {
+			System.out.println("우리는 구글과 페이스북만 지원한다");
+		}
 		
-		String provider = userRequest.getClientRegistration().getClientId();//google
-		String providerId = oAuth2User.getAttribute("sub");
+		String provider = oAuth2Userinfo.getProvider();
+		String providerId = oAuth2Userinfo.getProviderId();
 		String username = provider+"_"+providerId; //google_105617652356040885552이렇게 됨
 		//이렇게 되면 username이 충돌 날일이 없다.
 		String password =bCryptPasswordEncoder.encode("25g");
-		String email = oAuth2User.getAttribute("email");
+		String email = oAuth2Userinfo.getEmail();
 		String role = "ROLE_USER";
 		//아무나 가입시키거나 중복가입하면 안되기때문에 체크를해줘야함
 		User userEntity = userRepository.findByUsername(username);
 		
 		if(userEntity ==null) {
 		
-			System.out.println("당신은 최초 구글로그인 사용자 입니다 자동 회원가입을 진행합니다.");
+			System.out.println("당신은 최초 oauth로그인 사용자 입니다 자동 회원가입을 진행합니다.");
 			
 			userEntity = User.builder()
 					.username(username)
@@ -59,7 +72,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 					.build();
 			userRepository.save(userEntity);
 		}else {
-			System.out.println("구글 로그인을 이미 한적이 있습니다. 이미 지동회원가입이 되어있습니다.");
+			System.out.println("oauth로그인을 이미 한적이 있습니다. 이미 지동회원가입이 되어있습니다.");
 		}
 		
 		return new PrincipalDetails(userEntity,oAuth2User.getAttributes());
